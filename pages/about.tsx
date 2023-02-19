86626c8b6cfc013d812f2b5795ee4489fcbcba28
Jet <jethro.talavera1@gmail.com>
2023-02-18 21:18:43 -0500

    initial-changes

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import styles from '@/styles/Home.module.css'
+
+export default function About() {
+    return (
+      <>
+
+        <main className={styles.main}>
+          <h1>This is the about page</h1>
+        </main>
+      </>
+    )
+  }
