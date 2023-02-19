4268633703ae0f37d49ea113d4380e102f6decc2
Jet <jethro.talavera1@gmail.com>
2023-02-19 14:21:55 -0500

    styling-convention-new-pages

---
+++ pages/tracking/TrackingPage.tsx
@@ -0,0 +1,13 @@
+import { MainWrapper } from "./Styles";
+
+
+
+export default function TrackingPage() {
+    return (
+
+        <MainWrapper>
+          <h1>Lets add the tracking here</h1>
+        </MainWrapper>
+
+    )
+  }
